d622d221c2397ad56a0cab4eff6f850bd563e14a
Jose Miguel Arquelladas <jmaruiz@gmail.com>
2018-03-19 07:35:50 +0100

    Avance gestor.js y gestor.html
    Finalizado efecto acordeon en configuraciones

--- js/gestor.js
+++ js/gestor.js
@@ -9,6 +9,8 @@
  */
 
 $(document).ready(function() {
+        
+    /* Comentario del evento */
     $('#menu_ini' ).click(function(){
         $('.pestania').each(function(){
             $(this).hide();
@@ -16,7 +18,8 @@ $(document).ready(function() {
         $('#titulo').text('Inicio')
         $('#opc_ini').show();
     });
-
+        
+    /* Comentario del evento */
     $('#menu_emp, #menu_emp_sol').click(function(){
         $('.pestania').each(function(){
             $(this).hide();
@@ -24,7 +27,8 @@ $(document).ready(function() {
         $('#titulo').text('Empleados')
         $('#opc_emp').show();
     });
-
+        
+    /* Comentario del evento */
     $('#menu_sol, #menu_sol_sol').click(function(){
         $('.pestania').each(function(){
             $(this).hide();
@@ -32,7 +36,8 @@ $(document).ready(function() {
         $('#titulo').text('Solicitudes')
         $('#opc_sol').show();
     });
-
+        
+    /* Comentario del evento */
     $('#menu_pre, #menu_pre_sol').click(function(){
         $('.pestania').each(function(){
             $(this).hide();
@@ -40,7 +45,8 @@ $(document).ready(function() {
         $('#titulo').text('Control de presencia')
         $('#opc_pre').show();
     });
-
+        
+    /* Comentario del evento */
     $('#menu_cal, #menu_cal_sol').click(function(){
         $('.pestania').each(function(){
             $(this).hide();
@@ -48,7 +54,8 @@ $(document).ready(function() {
         $('#titulo').text('Calendarios')
         $('#opc_cal').show();
     });
-
+        
+    /* Comentario del evento */
     $('#menu_tur_jor, #menu_tur_jor_sol').click(function(){
         $('.pestania').each(function(){
             $(this).hide();
@@ -56,7 +63,8 @@ $(document).ready(function() {
         $('#titulo').text('Jornadas y Turnos')
         $('#opc_tur_jor').show();
     });
-
+        
+    /* Comentario del evento */
     $('#menu_par, #menu_par_sol').click(function(){
         $('.pestania').each(function(){
             $(this).hide();
@@ -64,24 +72,100 @@ $(document).ready(function() {
         $('#titulo').text('Parámetros generales')
         $('#opc_par').show();
     });
-    
+        
+    /* Comentario del evento */
     $('#camPosiV_cal').click(function(){
         $('#calHoriz').hide();
         $('#calVerti').show();
     });
-    
+        
+    /* Comentario del evento */
     $('#camPosiH_cal').click(function(){
         $('#calVerti').hide();
         $('#calHoriz').show();
     });
-    
+        
+    /* Comentario del evento */
     $('#camPosiV_tur_jor').click(function(){
         $('#jor_turHoriz').hide();
         $('#jor_turVerti').show();
     });
-    
+        
+    /* Comentario del evento */
     $('#camPosiH_tur_jor').click(function(){
         $('#jor_turVerti').hide();
         $('#jor_turHoriz').show();
+    });
+        
+    /* Comentario del evento */
+    $('#are_apar').click(function(){
+        if($('#con_are').hasClass('w3-hide')) { // Si está oculto
+            // ocultamos los demas elementos de la clase configuraciones
+            $('.configuraciones').each(function(){
+                $(this).removeClass("w3-show");
+                $(this).addClass("w3-hide");
+            });
+            // mostramos el contenido del apartado
+            $('#con_are').removeClass("w3-hide");
+            $('#con_are').addClass("w3-show");
+        } else { // Está mostrandose el contenido
+            // Lo ocultamos automáticamente
+            $('#con_are').removeClass("w3-show");
+            $('#con_are').addClass("w3-hide");
+        }
+    });
+        
+    /* Comentario del evento */
+    $('#per_apar').click(function(){
+        if($('#con_per').hasClass('w3-hide')) { // Si está oculto
+            // ocultamos los demas elementos de la clase configuraciones
+            $('.configuraciones').each(function(){
+                $(this).removeClass("w3-show");
+                $(this).addClass("w3-hide");
+            });
+            // mostramos el contenido del apartado
+            $('#con_per').removeClass("w3-hide");
+            $('#con_per').addClass("w3-show");
+        } else { // Está mostrandose el contenido
+            // Lo ocultamos automáticamente
+            $('#con_per').removeClass("w3-show");
+            $('#con_per').addClass("w3-hide");
+        }
+    });
+        
+    /* Comentario del evento */
+    $('#rol_apar').click(function(){
+        if($('#con_rol').hasClass('w3-hide')) { // Si está oculto
+            // ocultamos los demas elementos de la clase configuraciones
+            $('.configuraciones').each(function(){
+                $(this).removeClass("w3-show");
+                $(this).addClass("w3-hide");
+            });
+            // mostramos el contenido del apartado
+            $('#con_rol').removeClass("w3-hide");
+            $('#con_rol').addClass("w3-show");
+        } else { // Si está mostrandose el contenido
+            // Lo ocultamos automáticamente
+            $('#con_rol').removeClass("w3-show");
+            $('#con_rol').addClass("w3-hide");
+        }
+    });
+        
+    /* Comentario del evento */
+    $('#log_apar').click(function(){
+        if($('#con_log').hasClass('w3-hide')) { // Si está oculto
+            // ocultamos los demas elementos de la clase configuraciones
+            $('.configuraciones').each(function(){
+                $(this).removeClass("w3-show");
+                $(this).addClass("w3-hide");
+            });
+            // mostramos el contenido del apartado
+            $('#con_log').removeClass("w3-hide");
+            $('#con_log').addClass("w3-show");
+        } else { // Está mostrandose el contenido
+            // Lo ocultamos automáticamente
+            $('#con_log').removeClass("w3-show");
+            $('#con_log').addClass("w3-hide");
+        }
     });
 });
